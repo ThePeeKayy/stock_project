8bce15a222ef649720cca7f05eb9c5cf61acee00
pengkiang <pengkiangtan@gmail.com>
2025-12-01 18:53:27 +0800

    Fix hero2

--- app/page.js
+++ app/page.js
@@ -46,10 +46,7 @@ export default function StockBotHero() {
           Predict Stock Market Trends with <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-600">Stockly</span>
         </h1>
 
-<div
-        className=" absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
-        aria-hidden="true"
-      >
+
         <div
           className="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
           style={{
@@ -57,7 +54,6 @@ export default function StockBotHero() {
               'polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)',
           }}
         />
-      </div>
 
         <div className="flex flex-wrap justify-center gap-4 mt-16">
           {stockSymbols.map((stock, index) => {
